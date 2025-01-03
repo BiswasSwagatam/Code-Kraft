af3fbcb1fe245925cadfec110a4602c4e22dbded
BiswasSwagatam <swagatam.04.biswas@gmail.com>
2025-01-03 18:58:52 +0530

    .

---
+++ src/app/snippets/[id]/_components/Comments.tsx
@@ -0,0 +1,7 @@
+import { Id } from "../../../../../convex/_generated/dataModel";
+
+function Comments({ snippetId }: { snippetId: Id<"snippets"> }) {
+  return <div>Comments</div>;
+}
+
+export default Comments;
